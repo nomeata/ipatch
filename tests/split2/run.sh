d9cf57c3a65c25cfc36b1423f35241aefb76d69b
Joachim Breitner <mail@joachim-breitner.de>
2010-08-22 17:56:13 +0000

    Test case for splitting with file adding/removing

---
+++ tests/split2/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+cd $(dirname $0)
+
+if test -x ../../dist/build/ipatch/ipatch
+then IPATCH=$PWD/../../dist/build/ipatch/ipatch
+else IPATCH=ipatch
+fi
+
+rm -rf tmp
+
+cp -r before/ tmp
+cd tmp
+export PATH="..:$PATH"
+( echo yynnpatches/featureA.patch
+  echo yynpatches/addfile.patch
+  echo yypatches/removefile.patch
+)| $IPATCH split patches/testpatch
+
+perl -p -i -e 's/\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d.\d+ .\d\d\d\d/ignored date/' patches/featureA.patch patches/addfile.patch patches/removefile.patch
+cd ..
+diff -r tmp after
+rm -rf tmp
+echo "Test done"
+
